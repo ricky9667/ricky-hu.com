f25888b409803d683fa912fd99e2e252ebec8211
Ricky Hu <ricky9667@gmail.com>
2022-01-23 14:46:32 +0800

    add link to icons

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,19 @@
 <template>
-<!--  <img alt="Vue logo" src="./assets/logo.png" />-->
-<!--  <HelloWorld msg="Hello Vue 3 + TypeScript + Vite" />-->
-  <Home />
+  <div id="navbar" class="h-16 px-8 py-0">
+    <h3 class="text-white float-left">🇹🇼 Ricky Hu</h3>
+    <h3 class="text-white float-right">About</h3>
+  </div>
+  <Home class="my-8"/>
 </template>
 
 <script lang="ts">
-import { defineComponent } from "vue";
-import HelloWorld from "./components/HelloWorld.vue";
+import {defineComponent} from "vue";
 import Home from "./components/Home.vue";
 
 export default defineComponent({
   name: "App",
   components: {
     Home,
-    HelloWorld,
   },
 });
 </script>
@@ -22,6 +22,9 @@ export default defineComponent({
 html, body {
   margin: 0;
   padding: 0;
+  height: 100vh;
+  background-color: #2c3e50;
+  background-image: url("data:image/svg+xml,%3Csvg width='64' height='64' viewBox='0 0 64 64' xmlns='http://www.w3.org/2000/svg'%3E%3Cpath d='M8 16c4.418 0 8-3.582 8-8s-3.582-8-8-8-8 3.582-8 8 3.582 8 8 8zm0-2c3.314 0 6-2.686 6-6s-2.686-6-6-6-6 2.686-6 6 2.686 6 6 6zm33.414-6l5.95-5.95L45.95.636 40 6.586 34.05.636 32.636 2.05 38.586 8l-5.95 5.95 1.414 1.414L40 9.414l5.95 5.95 1.414-1.414L41.414 8zM40 48c4.418 0 8-3.582 8-8s-3.582-8-8-8-8 3.582-8 8 3.582 8 8 8zm0-2c3.314 0 6-2.686 6-6s-2.686-6-6-6-6 2.686-6 6 2.686 6 6 6zM9.414 40l5.95-5.95-1.414-1.414L8 38.586l-5.95-5.95L.636 34.05 6.586 40l-5.95 5.95 1.414 1.414L8 41.414l5.95 5.95 1.414-1.414L9.414 40z' fill='%239C92AC' fill-opacity='0.4' fill-rule='evenodd'/%3E%3C/svg%3E");
 }
 
 #app {
--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="home">
+  <div id="home">
     <img
       src="https://www.gravatar.com/avatar/ac9998c597a87af978beb083443f8eb7?s=800"
       alt="Me"
@@ -8,42 +8,14 @@
 
     <h1 class="text-white">Ricky Hu</h1>
     <h4 class="text-white">Programming / Speedcubing / Table Tennis / Cats</h4>
-    <div class="flex flex-row justify-center gap-4">
-      <img
-        src="https://play-lh.googleusercontent.com/ccWDU4A7fX1R24v-vvT480ySh26AYp97g1VrIB_FIdjRcuQB2JP2WdY7h_wVVAeSpg=s360"
-        alt="Facebook"
-        class="link-icon"
-      >
-      <img
-        src="https://play-lh.googleusercontent.com/h9jWMwqb-h9hjP4THqrJ50eIwPekjv7QPmTpA85gFQ10PjV02CoGAcYLLptqd19Sa1iJ=s360"
-        alt="Instagram"
-        class="link-icon"
-      >
-      <img
-        src="https://play-lh.googleusercontent.com/ZU9cSsyIJZo6Oy7HTHiEPwZg0m2Crep-d5ZrfajqtsH-qgUXSqKpNA2FpPDTn-7qA5Q=s360"
-        alt="Telegram"
-        class="link-icon"
-      >
-      <img
-        src="https://play-lh.googleusercontent.com/PCpXdqvUWfCW1mXhH1Y_98yBpgsWxuTSTofy3NGMo9yBTATDyzVkqU580bfSln50bFU=s360"
-        alt="Github"
-        class="link-icon"
-      >
-      <img
-        src="https://play-lh.googleusercontent.com/kMofEFLjobZy_bCuaiDogzBcUT-dz3BBbOrIEjJ-hqOabjK8ieuevGe6wlTD15QzOqw=s360"
-        alt="LinkedIn"
-        class="link-icon"
-      >
-      <img
-        src="https://play-lh.googleusercontent.com/6YKvTYU0qunPIsv7BvlQTrIKqbczaG75-POrOlVRdXdHlWqj3grpRs-MkkK6GJrVqjs=s360"
-        alt="Twitter"
-        class="link-icon"
-      >
-      <img
-        src="https://www.worldcubeassociation.org/assets/WCA%20Logo%202020-dacb67d012ad8fdf646e78159e7fcac40d11dab6b3828e790b00fbf67e4dcb88.svg"
-        alt="WCA"
-        class="link-icon"
-      >
+    <div class="flex flex-row justify-center gap-2 md:gap-4">
+      <a v-for="(link, index) in socialLinks" :key="index" :href="link.url" target="_blank" class="link-icon-animation">
+        <img
+          :src="link.image"
+          :alt="link.name"
+          class="link-icon-image"
+        >
+      </a>
     </div>
   </div>
 </template>
@@ -53,23 +25,62 @@ import {defineComponent} from "vue";
 
 export default defineComponent({
   name: "Home",
+  setup() {
+    const socialLinks = [
+      {
+        name: "Facebook",
+        url: "https://www.facebook.com/ricky.hu.0110/",
+        image: "https://play-lh.googleusercontent.com/ccWDU4A7fX1R24v-vvT480ySh26AYp97g1VrIB_FIdjRcuQB2JP2WdY7h_wVVAeSpg=s360"
+      },
+      {
+        name: "Instagram",
+        url: "https://www.instagram.com/ricky_hu_0110/",
+        image: "https://play-lh.googleusercontent.com/h9jWMwqb-h9hjP4THqrJ50eIwPekjv7QPmTpA85gFQ10PjV02CoGAcYLLptqd19Sa1iJ=s360"
+      },
+      {
+        name: "Github",
+        url: "https://github.com/ricky9667/",
+        image: "https://play-lh.googleusercontent.com/PCpXdqvUWfCW1mXhH1Y_98yBpgsWxuTSTofy3NGMo9yBTATDyzVkqU580bfSln50bFU=s360"
+      },
+      {
+        name: "Telegram",
+        url: "https://t.me/ricky9667/",
+        image: "https://play-lh.googleusercontent.com/ZU9cSsyIJZo6Oy7HTHiEPwZg0m2Crep-d5ZrfajqtsH-qgUXSqKpNA2FpPDTn-7qA5Q=s360"
+      },
+      {
+        name: "LinkedIn",
+        url: "https://www.linkedin.com/in/ricky9667/",
+        image: "https://play-lh.googleusercontent.com/kMofEFLjobZy_bCuaiDogzBcUT-dz3BBbOrIEjJ-hqOabjK8ieuevGe6wlTD15QzOqw=s360"
+      },
+      {
+        name: "Twitter",
+        url: "https://twitter.com/ricky9667/",
+        image: "https://play-lh.googleusercontent.com/6YKvTYU0qunPIsv7BvlQTrIKqbczaG75-POrOlVRdXdHlWqj3grpRs-MkkK6GJrVqjs=s360"
+      },
+      {
+        name: "WCA",
+        url: "https://worldcubeassociation.org/persons/2015HUSH02",
+        image: "https://www.worldcubeassociation.org/assets/WCA%20Logo%202020-dacb67d012ad8fdf646e78159e7fcac40d11dab6b3828e790b00fbf67e4dcb88.svg"
+      }
+    ];
+
+    return {socialLinks}
+  }
 });
 </script>
 
 <style scoped>
-.home {
-  height: 100vh;
-  background-color: #2c3e50;
-  //background-image: url("https://ik.imagekit.io/pxhytijjnsj/Temp/152530761_3648777875237713_503035110259364789_n_zXjswZWEz.jpg?ik-sdk-version=javascript-1.4.3&updatedAt=1642855775326");
-  //background-position: center;
-}
-
 .profile-image {
   @apply rounded-full w-32 md:w-48 lg:w-60;
 }
 
-.link-icon {
+.link-icon-animation {
+  @apply transform hover:-translate-y-1 transition duration-150;
+}
+
+.link-icon-image {
   @apply w-10;
   @apply rounded-full;
+  @apply transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110;
 }
 </style>
