6f59fa9d02d1f31bf0287e596893635d0741f1eb
Ricky Hu <ricky9667@gmail.com>
2022-01-23 13:28:34 +0800

    add basic layout

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,34 @@
-<script setup lang="ts">
-// This starter template is using Vue 3 <script setup> SFCs
-// Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3 + TypeScript + Vite" />
+<!--  <img alt="Vue logo" src="./assets/logo.png" />-->
+<!--  <HelloWorld msg="Hello Vue 3 + TypeScript + Vite" />-->
+  <Home />
 </template>
 
+<script lang="ts">
+import { defineComponent } from "vue";
+import HelloWorld from "./components/HelloWorld.vue";
+import Home from "./components/Home.vue";
+
+export default defineComponent({
+  name: "App",
+  components: {
+    Home,
+    HelloWorld,
+  },
+});
+</script>
+
 <style>
+html, body {
+  margin: 0;
+  padding: 0;
+}
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
---
+++ src/components/Home.vue
@@ -0,0 +1,75 @@
+<template>
+  <div class="home">
+    <img
+      src="https://www.gravatar.com/avatar/ac9998c597a87af978beb083443f8eb7?s=800"
+      alt="Me"
+      class="profile-image"
+    >
+
+    <h1 class="text-white">Ricky Hu</h1>
+    <h4 class="text-white">Programming / Speedcubing / Table Tennis / Cats</h4>
+    <div class="flex flex-row justify-center gap-4">
+      <img
+        src="https://play-lh.googleusercontent.com/ccWDU4A7fX1R24v-vvT480ySh26AYp97g1VrIB_FIdjRcuQB2JP2WdY7h_wVVAeSpg=s360"
+        alt="Facebook"
+        class="link-icon"
+      >
+      <img
+        src="https://play-lh.googleusercontent.com/h9jWMwqb-h9hjP4THqrJ50eIwPekjv7QPmTpA85gFQ10PjV02CoGAcYLLptqd19Sa1iJ=s360"
+        alt="Instagram"
+        class="link-icon"
+      >
+      <img
+        src="https://play-lh.googleusercontent.com/ZU9cSsyIJZo6Oy7HTHiEPwZg0m2Crep-d5ZrfajqtsH-qgUXSqKpNA2FpPDTn-7qA5Q=s360"
+        alt="Telegram"
+        class="link-icon"
+      >
+      <img
+        src="https://play-lh.googleusercontent.com/PCpXdqvUWfCW1mXhH1Y_98yBpgsWxuTSTofy3NGMo9yBTATDyzVkqU580bfSln50bFU=s360"
+        alt="Github"
+        class="link-icon"
+      >
+      <img
+        src="https://play-lh.googleusercontent.com/kMofEFLjobZy_bCuaiDogzBcUT-dz3BBbOrIEjJ-hqOabjK8ieuevGe6wlTD15QzOqw=s360"
+        alt="LinkedIn"
+        class="link-icon"
+      >
+      <img
+        src="https://play-lh.googleusercontent.com/6YKvTYU0qunPIsv7BvlQTrIKqbczaG75-POrOlVRdXdHlWqj3grpRs-MkkK6GJrVqjs=s360"
+        alt="Twitter"
+        class="link-icon"
+      >
+      <img
+        src="https://www.worldcubeassociation.org/assets/WCA%20Logo%202020-dacb67d012ad8fdf646e78159e7fcac40d11dab6b3828e790b00fbf67e4dcb88.svg"
+        alt="WCA"
+        class="link-icon"
+      >
+    </div>
+  </div>
+</template>
+
+<script>
+import {defineComponent} from "vue";
+
+export default defineComponent({
+  name: "Home",
+});
+</script>
+
+<style scoped>
+.home {
+  height: 100vh;
+  background-color: #2c3e50;
+  //background-image: url("https://ik.imagekit.io/pxhytijjnsj/Temp/152530761_3648777875237713_503035110259364789_n_zXjswZWEz.jpg?ik-sdk-version=javascript-1.4.3&updatedAt=1642855775326");
+  //background-position: center;
+}
+
+.profile-image {
+  @apply rounded-full w-32 md:w-48 lg:w-60;
+}
+
+.link-icon {
+  @apply w-10;
+  @apply rounded-full;
+}
+</style>
